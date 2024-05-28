39da13d06cae8b0971b9e9541646cb46a4d2f7ad
heywinit <heywinit@gmail.com>
2024-05-29 00:11:58 +0530

    discord basic stuff

--- main.go
+++ main.go
@@ -1,5 +1,45 @@
 package main
 
-func main() {
+import (
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
 
+	"log"
+)
+
+func main() {
+	env, _ := godotenv.Read(".env")
+	token := env["DISCORD_TOKEN"]
+	if token == "" {
+		log.Fatal("No token provided")
+		return
+	}
+
+	discord, err := discordgo.New("Bot " + token)
+	if err != nil {
+		log.Fatal("Error creating Discord session: ", err)
+		return
+	}
+
+	discord.AddHandler(messageCreate)
+
+	err = discord.Open()
+	if err != nil {
+		log.Fatal("Error opening connection: ", err)
+		return
+	}
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	//ignore bot messages
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	if m.Content == "ping" {
+		_, err := s.ChannelMessageSend(m.ChannelID, "Pong!")
+		if err != nil {
+			return
+		}
+	}
 }
