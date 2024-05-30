695f950e31a4cc9bf8da75d263deae506bb2c6b9
heywinit <heywinit@gmail.com>
2024-05-30 19:30:49 +0530

    discord storing progress

--- discodb/database.go
+++ discodb/database.go
@@ -1,15 +1,22 @@
 package discodb
 
 import (
+	"encoding/json"
 	"errors"
 	"github.com/bwmarrin/discordgo"
 )
+
+type IDStore struct {
+	Categories map[string]string
+	Channels   map[string]string
+}
 
 type Database struct {
 	Token   string
 	GuildId string
 	Tables  []*Table
 	Client  *discordgo.Session
+	IdStore IDStore
 }
 
 type Column struct {
@@ -18,11 +25,12 @@ type Column struct {
 	Unique bool
 }
 
+// Table TODO: now that im thinking of it, we might use something like mongo does with bson. so you can give a struct, and it serializes that into something and stores that. but that would add overhead. anyways, its 1am i should go sleep
 type Table struct {
-	Database *Database
-	Name     string
-	Columns  []Column
-	Rows     []map[string]interface{}
+	Database *Database                `json:"database"`
+	Name     string                   `json:"name"`
+	Columns  []Column                 `json:"columns"`
+	Rows     []map[string]interface{} `json:"rows"`
 }
 
 func NewDatabase(token, guildId string) (*Database, error) {
@@ -39,11 +47,43 @@ func NewDatabase(token, guildId string) (*Database, error) {
 		return nil, err
 	}
 
+	idStore := IDStore{}
+	categories := make(map[string]string)
+
+	tableCategory, err := client.GuildChannelCreate(guildId, "tables", discordgo.ChannelTypeGuildCategory)
+	if err != nil {
+		return nil, err
+	}
+	categories["tables"] = tableCategory.ID
+
+	discoDBCategory, err := client.GuildChannelCreate(guildId, "discodb", discordgo.ChannelTypeGuildCategory)
+	if err != nil {
+		return nil, err
+	}
+	categories["discodb"] = discoDBCategory.ID
+
+	channels := make(map[string]string)
+	tablesChannel, err := client.GuildChannelCreate(guildId, "tables", discordgo.ChannelTypeGuildText)
+	if err != nil {
+		return nil, err
+	}
+	_, err = client.ChannelEditComplex(tablesChannel.ID, &discordgo.ChannelEdit{
+		ParentID: discoDBCategory.ID,
+	})
+	if err != nil {
+		return nil, err
+	}
+	channels["tables"] = tablesChannel.ID
+
+	idStore.Categories = categories
+	idStore.Channels = channels
+
 	return &Database{
 		Token:   token,
 		GuildId: guildId,
 		Tables:  make([]*Table, 0),
 		Client:  client,
+		IdStore: idStore,
 	}, nil
 }
 
@@ -55,9 +95,56 @@ func (db *Database) CreateTable(name string, columns []Column) (*Table, error) {
 	}
 	db.Tables = append(db.Tables, table)
 
-	//TODO: implement a few channels under discodb category. those shall act as an internal storage. store the id of this channel in that channel
-	_, err := db.Client.GuildChannelCreate(db.GuildId, name, discordgo.ChannelTypeGuildText)
+	createdTable, err := db.Client.GuildChannelCreate(db.GuildId, name, discordgo.ChannelTypeGuildText)
+	if err != nil {
+		return nil, err
+	}
+	_, err = db.Client.ChannelEditComplex(createdTable.ID, &discordgo.ChannelEdit{
+		ParentID: db.IdStore.Categories["tables"],
+	})
+	if err != nil {
+		return nil, err
+	}
 
+	//serialize the created table into json
+	tableJson, _ := json.Marshal(table)
+
+	//get tables channel
+	tablesChannel, err := db.Client.Channel(db.IdStore.Channels["tables"])
+	if err != nil {
+		return nil, err
+	}
+	messages, err := db.Client.ChannelMessages(tablesChannel.ID, 1, "", "", "")
+	if err != nil {
+		return nil, err
+	}
+
+	//if no other tables exist
+	if len(messages) == 0 {
+		tables := []string{string(tableJson)}
+		tablesJson, _ := json.Marshal(tables)
+		_, err = db.Client.ChannelMessageSend(tablesChannel.ID, string(tablesJson))
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	//if other tables exist
+	if len(messages) > 0 {
+		var tables []string
+		err = json.Unmarshal([]byte(messages[0].Content), &tables)
+		if err != nil {
+			return nil, err
+		}
+		tables = append(tables, string(tableJson))
+		tablesJson, _ := json.Marshal(tables)
+		_, err = db.Client.ChannelMessageEdit(tablesChannel.ID, messages[0].ID, string(tablesJson))
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	db.Tables = append(db.Tables, table)
 	return table, err
 }
 
